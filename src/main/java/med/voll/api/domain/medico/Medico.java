49bf664bdbdfa5299aca72374b2cefd8429c0300
renanalencar02 <renanalencar02@gmail.com>
2023-11-17 16:15:27 -0300

    primeira parte do projeto vollmed

---
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -0,0 +1,64 @@
+package med.voll.api.domain.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.endereco.Endereco;
+
+@Entity(name = "Medico")
+@Table(name = "medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    private boolean ativo;
+
+    public Medico(String nome){
+        this.nome = nome;
+    }
+    public Medico(DadosCadastroMedico dados){
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.telefone = dados.telefone();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+
+        if (dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarEndereco(dados.endereco());
+        }
+    }
+
+    public void inativar() {
+
+        this.ativo = false;
+    }
+}
