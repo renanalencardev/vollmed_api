4f47b1a697437bbbd1fa1fecb83cb36970b59527
renanalencardev <renanalencar02@gmail.com>
2023-12-10 18:56:15 -0300

    criado o validador de agendamento de consulta com horário de antecedência minimo

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+        if(diferencaEmMinutos < 30){
+            throw new ValidacaoException("A consulta deve ser agendada com no mínimo 30 minutos de antecedência");
+        }
+    }
+}
