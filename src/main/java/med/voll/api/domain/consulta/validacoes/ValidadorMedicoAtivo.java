4ea9739db75c67526a177f3e4d95dab09f0be4c1
renanalencardev <renanalencar02@gmail.com>
2023-12-10 18:58:48 -0300

    criado validador de médico ativo

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ValidadorMedicoAtivo {
+
+    private MedicoRepository medicoRepository;
+    public void validar(DadosAgendamentoConsulta dados){
+        if(dados.idMedico() == null){
+            return;
+        }
+        var medicoEstaAtivo = medicoRepository.findAtivoById(dados.idMedico());
+        if(!medicoEstaAtivo){
+            throw new ValidacaoException("Consulta não pode ser agendada com médico excluído");
+        }
+    }
+}
