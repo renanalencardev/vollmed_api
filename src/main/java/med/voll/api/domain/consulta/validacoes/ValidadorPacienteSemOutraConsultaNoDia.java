49831f13e385d0199baef12a6ee6d6549fdc36f2
renanalencardev <renanalencar02@gmail.com>
2023-12-10 18:59:23 -0300

    criado validador de paciente sem outra consulta no mesmo dia

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorPacienteSemOutraConsultaNoDia {
+
+    private ConsultaRepository consultaRepository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = consultaRepository.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+    }
+}
